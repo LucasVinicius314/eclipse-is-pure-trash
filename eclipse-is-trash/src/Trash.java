4a2592a3a065604a39b71f1ee27be6014ca2381a
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-09-02 21:59:23 -0300

    trash

---
+++ eclipse-is-trash/src/Trash.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Trash {
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		
+		System.out.println("First number: ");
+		var a = sc.nextInt();
+		System.out.println("Second number");
+		var b = sc.nextInt();
+		System.out.println("Sum: " + (a + b));
+		System.out.println("enough, now uninstall this and install this https://code.visualstudio.com/");
+		
+		sc.close();
+	}
+}
